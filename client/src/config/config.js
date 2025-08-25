e439484969b41dc555166e1b63e8da4c50af99a5
mannpatel <mannpatel7744@gmail.com>
2025-08-25 16:10:56 +0530

    Changing to Azure backend for testing

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -5,10 +5,10 @@ const config = {
         CLIENT_URL: 'http://localhost:5173'
     },
     production: {
-        API_BASE_URL: 'https://cloud-storage-project-backend.onrender.com/api/',
+        // API_BASE_URL: 'https://cloud-storage-project-backend.onrender.com/api/',
         // CLIENT_URL: 'https://cloud-storage-project-frontend.onrender.com/'
         CLIENT_URL: 'https://red-glacier-0eaf9250f.1.azurestaticapps.net',
-        // API_BASE_URL:'https://cloud-project-backend-c4d6cmancdcucbcg.eastus2-01.azurewebsites.net'
+        API_BASE_URL:'https://cloud-project-backend-c4d6cmancdcucbcg.eastus2-01.azurewebsites.net/api/'
     }
 };
 
